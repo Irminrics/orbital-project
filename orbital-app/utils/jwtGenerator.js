b3194e3b8f36f5c2d67efbbad680a2609996a128
Jun Kang <angjunkang@u.nus.edu>
2022-06-08 23:22:41 +0800

    Heroku Deployment V2
    Updated JWT environmental variable

--- orbital-app/utils/jwtGenerator.js
+++ orbital-app/utils/jwtGenerator.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 function jwtGenerator(id) {
-    return jwt.sign({user: id}, process.env.jwtSecret, { expiresIn: "1h" });
+    console.log(process.env.jwtSecret);
+    return jwt.sign({ user: id }, process.env.JWT_SECRET, { expiresIn: "1h" });
 }
 
 module.exports = jwtGenerator;
