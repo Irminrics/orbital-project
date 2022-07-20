73415bf0e2b9010b733654eee308dc2dad89caa1
Jun Kang <angjunkang@u.nus.edu>
2022-07-20 20:46:22 +0800

    Insert loading for projects page

--- orbital-app/client/src/pages/projects.jsx
+++ orbital-app/client/src/pages/projects.jsx
@@ -7,6 +7,7 @@ import '../css/homepage.css';
 import Navigation from "../components/Header/Navigation";
 import ProjectTitle from "../components/Projects/ProjectTitle";
 import ProjectContent from "../components/Projects/ProjectContent";
+import LoadingSpinner from '../components/LoadingSpinner'
 import Footer from "../components/Footer/Footer";
 
 import React, { useEffect, useState } from "react";
@@ -35,6 +36,7 @@ const ProjectsPage = ({ setAuth, isAuthenticated }) => {
 
 const Project = () => {
     const [projects, setProjects] = useState([]);
+    const [isLoading, setLoading] = useState(true);
     let { achievement } = useParams();
 
 
@@ -43,6 +45,7 @@ const Project = () => {
             const response = await fetch(`/projects/achievement/${achievement}`);
             const jsonData = await response.json();
             setProjects(jsonData);
+            setLoading(false);
         } catch (err) {
             console.error(err.message);
         }
@@ -52,9 +55,10 @@ const Project = () => {
         getProjects(achievement);
     }, [])
 
+
     if (projects.length === 0) {
         return (
-            <>
+            isLoading ? <LoadingSpinner /> : <>
                 <div className='emptyProject'>
                     <p>There are no projects available.</p>
                 </div>
@@ -62,7 +66,7 @@ const Project = () => {
         )
     } else {
         return (
-            <>
+            isLoading ? <LoadingSpinner /> : <>
                 {projects.map(project => (
                     <div key={project.id}>
                         <ProjectContent teamName={project.teamname} teamID={project.id} teamMember1={project.teammember1} teamMember2={project.teammember2} teamAdvisor={project.teamadvisor}
